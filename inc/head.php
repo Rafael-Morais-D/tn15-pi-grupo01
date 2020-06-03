3b803efa0d69346a34f7cc2a897bf029ea56f6eb
thales-feitosa <contact.thales@gmail.com>
2020-06-03 17:30:01 -0300

    Adicionando Metatags para o SEO

--- inc/head.php
+++ inc/head.php
@@ -10,4 +10,84 @@
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.9.0/css/all.css" integrity="sha384-vlOMx0hKjUCl4WzuhIhSNZSm2yQCaf0mOU1hEDK/iztH3gU4v5NMmJln9273A6Jz" crossorigin="anonymous">
     <!-- Style CSS -->
     <link rel="stylesheet" href="./assets/css/style.css">
+    <!-- Metatags - SEO -->
+    <script type="application/ld+json">
+    {
+        "@context": "https://schema.org",
+        "@type": "Organization",
+        "name": "Bake & Go",
+        "logo": "#",
+        "url": "#",
+        "description": "Bake & Go, a sua padaria online.",
+        "email": "bakeandgo.coffeeshop@gmail.com",
+        "address": {
+            "@type": "PostalAddress",
+            "addressCountry": "BR",
+            "addressLocality": "São Paulo",
+            "addressRegion": "São Paulo",
+            "postalCode": "04548-000",
+            "streetAddress": "Av. Dr. Cardoso de Melo, 90 - Vila Olímpia"
+        },
+        "sameAs": ["https://www.instagram.com/", "https://www.facebook.com/"],
+        "brand": [
+                { "@type": "brand", "name": "Bake & Go", "url": "#" }
+        ],
+    }
+    </script>
+
+    <meta name="title" content="Bake & Go">
+    <meta name="description" content="Bake & Go, a sua padaria online.">
+    <meta name="identifier-URL" content="#">
+    <meta name="url" content="#">
+    <meta name="abstract" content="Bake & Go">
+    <meta name="author" content="Bake & Go">
+    <meta name="robots" content="index,follow">
+    <meta name="contact" content="bakeandgo.coffeeshop@gmail.com">
+    <meta name="reply-to" content="bakeandgo.coffeeshop@gmail.com">
+    <meta name="copyright" content="Bake & Go">
+    <meta name="rating" content="general">
+    <meta name="web_author" content="Bake & Go">
+    <meta property="og:site_name" content="Alt Studio">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Bake & Go">
+    <meta property="og:description" content="Bake & Go, a sua padaria online.">
+    <meta property="og:url" content="#">
+    <meta property="og:locale" content="pt-BR">
+    <meta name="og:locality" content="São Paulo">
+    <meta name="og:region" content="SP">
+    <meta name="og:country-name" content="BR">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Bake & Go">
+    <meta name="twitter:description" content="#">
+    <meta name="twitter:image" content="#">
+    <meta name="keywords" content="Bake & Go, padaria, café, coffee shop, pão artesanal, pão italiano, pão de queijo">
+    <meta property="og:type" content="website">
+    <meta name="application-name" content="Bake & Go">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-title" content="Bake & Go">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <meta name="msapplication-TileColor" content="#faf8f3">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000">
+    <!--[if IE]><link rel="shortcut icon" href="#"><![endif]-->
+    <meta name="msapplication-TileImage" content="#">
+    <link rel="icon" type="image/png" sizes="512x512" href="#">
+    <link rel="icon" type="image/png" sizes="228x228" href="#">
+    <link rel="shortcut icon" sizes="196x196" href="#">
+    <link rel="icon" sizes="192x192" href="#">
+    <link rel="apple-touch-icon-precomposed" sizes="180x180" href="#">
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="#">
+    <link rel="icon" type="image/png" sizes="128x128" href="#">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="#">
+    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="#">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="#">
+    <link rel="icon" type="image/png" sizes="96x96" href="#">
+    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="#">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="#">
+    <link rel="apple-touch-icon-precomposed" sizes="60x60" href="#">
+    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="#">
+    <link rel="icon" type="image/png" sizes="32x32" href="#">
+    <link rel="icon" type="image/png" sizes="16x16" href="#">
+</head>
 </head>
