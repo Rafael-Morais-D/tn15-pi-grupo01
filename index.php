c74f3ccad969d1487d79e34631486ab1f151919c
thales-feitosa <contact.thales@gmail.com>
2020-06-08 13:33:54 -0300

    Organizando a semântica do HTML na página inicial

--- index.php
+++ index.php
@@ -2,19 +2,20 @@
 <?php require_once("./inc/header.php"); ?>
 <!-- Main -->
 <main class="banner">
-    <div class="container d-flex h-100 align-items-center">
-        <div class="mx-auto text-center">
-            <h1 class="mx-auto my-0">Título</h1>
-            <h2 class="mx-auto mt-2 mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quaerat quo maiores libero veritatis, laboriosam dolor aspernatur maxime itaque, nulla ullam minus dignissimos nobis eligendi saepe. Quae amet et similique.</h2>
-        </div>
+  <div class="container d-flex h-100 align-items-center">
+    <div class="mx-auto text-center">
+      <h1 class="mx-auto my-0">Título</h1>
+      <h2 class="mx-auto mt-2 mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quaerat quo maiores libero veritatis, laboriosam dolor aspernatur maxime itaque, nulla ullam minus dignissimos nobis eligendi saepe. Quae amet et similique.</h2>
+      </div>
     </div>
 </main>
-  <!-- Section com produtos-->
-    <section>
-    <div class="col-lg-10 mx-auto col-12 text-center mb-3">
-              <h3 class="mt-5 mb-4 text-dark"><i class="fal fa-wheat mt-4 mr-3"></i>Conheça nossos produtos<i class="fal fa-wheat ml-3"></i>
-              <hr class="accent my-5">
-          </div>
+
+<!-- Section com produtos-->
+<section class="catProdutos">
+  <div class="col-lg-10 mx-auto col-12 text-center mb-3">
+    <h3 class="mt-5 mb-4 text-dark"><i class="fal fa-wheat mt-4 mr-3"></i>Conheça nossos produtos<i class="fal fa-wheat ml-3"></i></h3>
+    <hr class="accent my-5">
+    </div>
     <div class="row">
       <div class="col-lg-6 w-100 my-auto text-center text-lg-right">
         <h2>Pão Italiano</h2>
@@ -25,8 +26,6 @@
         <img class="img-fluid" src="./assets/img/03_bakeandgo.jpg" alt="">
       </div>
     </div>
-  </section>
-  <section>
     <div class="row">
       <div class="col-lg-6 w-100 my-auto text-center text-lg-left">
         <h2>Bolo de Chocolate com cobertura de Chocolate</h2>
@@ -39,112 +38,111 @@
     </div>
   </section>
 <!-- Menu -->
-  <aside>
-  <article>
+  <section class="menuProdutos">
     <div class="container">
       <div class="row-menu">
-          <div class="col-lg-10 mx-auto col-12 text-center mb-3">
-          <h3 class="mt-5 mb-4 text-dark"><i class="fal fa-wheat mt-4 mr-3"></i>Menu<i class="fal fa-wheat ml-3"></i>
-              <hr class="accent my-5">
+        <div class="col-lg-10 mx-auto col-12 text-center mb-3">
+          <h3 class="mt-5 mb-4 text-dark"><i class="fal fa-wheat mt-4 mr-3"></i>Menu<i class="fal fa-wheat ml-3"></i></h3>
+          <hr class="accent my-5">
           </div>
           <div class="card-columns">
-              <div class="card card-body">
-                  <span class="float-right">R$12</span>
-                  <h6 class="text-truncate">Pão Italiano</h6>
-                  <p class="small">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod inventore, voluptatibus fugiat eveniet atque optio? Facere sequi rerum error doloremque quas ea qui earum, tempora ex ipsa facilis quod itaque?</p>
-                  <label class="form-qtd" name="qtd">Qtd</label>
-                                    <select class="qtd mb-3" name="qtd">
-                                        <option selected value=1>1</option>
-                                        <option value=2>2</option>
-                                        <option value=3>3</option>
-                                        <option value=4>4</option>
-                                        <option value=5>5</option>
-                                        <option value=6>6</option>
-                                        <option value=7>7</option>
-                                        <option value=8>8</option>
-                                        <option value=9>9</option>
-                                        <option value=10>10</option>
-                                    </select>
-                  <a href="./cesta-de-compras.php" class="btn btn-primary float-right">COMPRAR</a>
+            <div class="card card-body">
+              <span class="float-right">R$12</span>
+              <h3 class="text-truncate">Pão Italiano</h3>
+              <p class="small">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod inventore, voluptatibus fugiat eveniet atque optio? Facere sequi rerum error doloremque quas ea qui earum, tempora ex ipsa facilis quod itaque?</p>
+              <label class="form-qtd" name="qtd">Qtd</label>
+              <select class="qtd mb-3" name="qtd">
+                <option selected value=1>1</option>
+                <option value=2>2</option>
+                <option value=3>3</option>
+                <option value=4>4</option>
+                <option value=5>5</option>
+                <option value=6>6</option>
+                <option value=7>7</option>
+                <option value=8>8</option>
+                <option value=9>9</option>
+                <option value=10>10</option>
+                </select>
+                <a href="./cesta-de-compras.php" class="btn btn-primary float-right">COMPRAR</a>
               </div>
               <div class="card card-body">
-                  <span class="float-right">R$8</span>
-                  <h6 class="text-truncate">Bagel</h6>
-                  <p class="small">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium porro a temporibus asperiores vitae alias, cumque eligendi deserunt, unde, quam saepe inventore distinctio mollitia sequi aliquam iusto sit facilis cupiditate!</p>
-                  <label class="form-inline-block" name="qtd">Qtd</label>
-                                    <select class="mb-3" name="qtd">
-                                        <option selected value=1>1</option>
-                                        <option value=2>2</option>
-                                        <option value=3>3</option>
-                                        <option value=4>4</option>
-                                        <option value=5>5</option>
-                                        <option value=6>6</option>
-                                        <option value=7>7</option>
-                                        <option value=8>8</option>
-                                        <option value=9>9</option>
-                                        <option value=10>10</option>
-                                    </select>
+                <span class="float-right">R$8</span>
+                <h3 class="text-truncate">Bagel</h3>
+                <p class="small">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium porro a temporibus asperiores vitae alias, cumque eligendi deserunt, unde, quam saepe inventore distinctio mollitia sequi aliquam iusto sit facilis cupiditate!</p>
+                <label class="form-qtd" name="qtd">Qtd</label>
+                <select class="qtd mb-3" name="qtd">
+                  <option selected value=1>1</option>
+                  <option value=2>2</option>
+                  <option value=3>3</option>
+                  <option value=4>4</option>
+                  <option value=5>5</option>
+                  <option value=6>6</option>
+                  <option value=7>7</option>
+                  <option value=8>8</option>
+                  <option value=9>9</option>
+                  <option value=10>10</option>
+                  </select>
                   <a href="./cesta-de-compras.php" class="btn btn-primary float-right">COMPRAR</a>
-              </div>
-              <div class="card card-body">
+                </div>
+                <div class="card card-body">
                   <span class="float-right">R$14</span>
-                  <h6 class="text-truncate">Cheesecake</h6>
+                  <h3 class="text-truncate">Cheesecake</h3>
                   <p class="small">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem facere nemo laboriosam architecto, magnam sunt veritatis neque autem quod libero illum unde eius expedita impedit sit perferendis. Ipsa, aliquam atque.</p>
                   <label class="form-inline-block" name="qtd">Qtd</label>
-                                    <select class="mb-3" name="qtd">
-                                        <option selected value=1>1</option>
-                                        <option value=2>2</option>
-                                        <option value=3>3</option>
-                                        <option value=4>4</option>
-                                        <option value=5>5</option>
-                                        <option value=6>6</option>
-                                        <option value=7>7</option>
-                                        <option value=8>8</option>
-                                        <option value=9>9</option>
-                                        <option value=10>10</option>
-                                    </select>
-                  <a href="./cesta-de-compras.php" class="btn btn-primary float-right">COMPRAR</a>
-              </div>
-              <div class="card card-body">
-                  <span class="float-right">R$10</span>
-                  <h6 class="text-truncate">Croissant</h6>
-                  <p class="small">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores delectus, iusto earum nesciunt similique nulla ullam hic explicabo! Explicabo ullam rem suscipit corporis, molestias non totam. Dolorum laboriosam debitis odit.</p>
-                  <label class="form-inline-block" name="qtd">Qtd</label>
-                                    <select class="mb-3" name="qtd">
-                                        <option selected value=1>1</option>
-                                        <option value=2>2</option>
-                                        <option value=3>3</option>
-                                        <option value=4>4</option>
-                                        <option value=5>5</option>
-                                        <option value=6>6</option>
-                                        <option value=7>7</option>
-                                        <option value=8>8</option>
-                                        <option value=9>9</option>
-                                        <option value=10>10</option>
-                                    </select>
-                  <a href="./cesta-de-compras.php" class="btn btn-primary float-right">COMPRAR</a>
-              </div>
-          </div>
-          </div>
-          <div class="col-12 mt-5 mb-5 text-center">
-            <a href="#" class="btn btn-primary">VER MENU</a>
-          </div>
-      </div>
-  </aside>
-</article>
-<hr class="accent my-5">
-<article>
-    <div class="row">
-      <div class="col-lg-6 w-100 my-auto mb-4 text-center text-lg-left">
-        <h3>Das nossas mão para sua mesa!</h3>
-        <p>Peça também pelo <a href="#" style="color: #8f793c!important;" class="wa-ic">WhatsApp<i style="color: #8f793c!important;" class="fab fa-whatsapp fa-lg text-dark ml-2"></a></i></p>
-        <p class="mt-4 mb-0">Atendimento e delivery</p>
-        <p class="mb-0">Segunda—Segunda</p>
-        <p>10h às 18h</p>
-      </div>
-      <div class="col-lg-6 order-lg-first mb-5">
-        <img class="img-fluid" src="./assets/img/07_bakeandgo.jpg" alt="">
-      </div>
-    </div>
-  </article>
+                  <select class="qtd mb-3" name="qtd">
+                    <option selected value=1>1</option>
+                    <option value=2>2</option>
+                    <option value=3>3</option>
+                    <option value=4>4</option>
+                    <option value=5>5</option>
+                    <option value=6>6</option>
+                    <option value=7>7</option>
+                    <option value=8>8</option>
+                    <option value=9>9</option>
+                    <option value=10>10</option>
+                    </select>
+                    <a href="./cesta-de-compras.php" class="btn btn-primary float-right">COMPRAR</a>
+                    </div>
+                    <div class="card card-body">
+                      <span class="float-right">R$10</span>
+                      <h3 class="text-truncate">Croissant</h3>
+                      <p class="small">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores delectus, iusto earum nesciunt similique nulla ullam hic explicabo! Explicabo ullam rem suscipit corporis, molestias non totam. Dolorum laboriosam debitis odit.</p>
+                      <label class="form-inline-block" name="qtd">Qtd</label>
+                      <select class="mb-3" name="qtd">
+                        <option selected value=1>1</option>
+                        <option value=2>2</option>
+                        <option value=3>3</option>
+                        <option value=4>4</option>
+                        <option value=5>5</option>
+                        <option value=6>6</option>
+                        <option value=7>7</option>
+                        <option value=8>8</option>
+                        <option value=9>9</option>
+                        <option value=10>10</option>
+                        </select>
+                        <a href="./cesta-de-compras.php" class="btn btn-primary float-right">COMPRAR</a>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-12 mt-5 mb-5 text-center">
+                    <a href="#" class="btn btn-primary">VER MENU</a>
+                    </div>
+                  </div>
+              </section>
+              <hr class="accent my-5">
+              <section class="catAtendimento">
+                <div class="row">
+                  <div class="col-lg-6 w-100 my-auto mb-4 text-center text-lg-left">
+                    <h3>Das nossas mão para sua mesa!</h3>          
+                    <p>Peça também pelo <a href="#" style="color: #8f793c!important;" class="wa-ic">WhatsApp<i style="color: #8f793c!important;" class="fab fa-whatsapp fa-lg text-dark ml-2"></a></i></p>
+                    <p class="mt-4 mb-0">Atendimento e delivery</p>
+                    <p class="mb-0">Segunda—Segunda</p>
+                    <p class="mb-5">10h às 18h</p>                    
+                    </div>
+                    <div class="col-lg-6 order-lg-first mb-5">
+                      <img class="img-fluid" src="./assets/img/07_bakeandgo.jpg" alt="">
+                    </div>
+                  </div>
+                </section>
+
 <?php require_once("./inc/footer.php"); ?>
