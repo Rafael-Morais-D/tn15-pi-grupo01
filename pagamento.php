3ad61bee5dfb8fc246d85abce11fc5a72ed3b2dd
RafaelMorais <rafaelmorais.design@gmail.com>
2020-06-11 22:33:07 -0300

    Criando página pagamento.php e corrigindo problema no footer

---
+++ pagamento.php
@@ -0,0 +1,8 @@
+<?php require_once("./inc/head.php"); ?>
+<?php require_once("./inc/header.php"); ?>
+
+    <main class="container-fluid py-5 mt-5">
+        
+    </main>
+
+<?php require_once("./inc/footer.php"); ?>
