b28a7d69726c2190ceaf726529789f8f3a08887e
thales-feitosa <contact.thales@gmail.com>
2020-06-25 16:27:23 -0300

    Corrigindo versão da página de erro

--- paginaErro.php
+++ paginaErro.php
@@ -1,16 +1,16 @@
+<?php $tituloPagina = "Bake & Go | 404 Não Encontrado" ?>
 <?php require_once("./inc/head.php"); ?>
 <?php require_once("./inc/header.php"); ?>
 <!-- Main -->
 <main class="error">
   <div class="container d-flex h-100 align-items-center">
-    <div class="mx-auto text-center">
-    <img class="img-fluid" src="./assets/img/404-pao.png" alt="">
-      <h1 class="mx-auto my-0">Desculpe!</h1>
-      <h2 class="mx-auto mt-2 mb-5"><br> Parece que não encontramos o que você procura.</h2>
+    <div class="mx-auto text-center animated animatedFadeInUp fadeInUp">
+    <img class="img-fluid error-img" src="./assets/img/404-pao.png" alt="">
+      <h2 class="mx-auto my-0">Desculpe!</h2>
+      <h2 class="mx-auto mb-5"><br> Parece que não encontramos o que você procura.</h2>
+      <div class="col-12 mt-5 mb-3 text-center">
+        <a href="./index.php" class="btn btn-primary">VOLTE A PÁGINA INICIAL</a>
+      </div>
       </div>
     </div>
 </main>
-
-
-
-<?php require_once("./inc/footer.php"); ?>
