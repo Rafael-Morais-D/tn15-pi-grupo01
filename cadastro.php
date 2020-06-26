ee9a18ce3bb1b57e25e2d94e4749d63d08dcb5f1
thales-feitosa <contact.thales@gmail.com>
2020-06-26 13:25:58 -0300

    Alterando a cor do checkbox na página de cadastro

--- cadastro.php
+++ cadastro.php
@@ -5,7 +5,7 @@
 <!-- Form - Cadastro -->
     <div class="container-fluid py-5 mt-5">
         <div class="form-cadastro">
-            <div class="col-md-6 mx-auto">
+            <div class="col-lg-6 mx-auto">
                     <div class="card card-body">
                         <h3 class="text-center mb-4">Criar uma conta</h3>
                         <fieldset>
@@ -64,10 +64,13 @@
                             <div class="form-group has-success">
                                 <input class="form-control input-lg" placeholder="Confirmar Senha" name="password" value="" type="password">
                             </div>
+                            
+
                             <div class="checkbox">
-                                <label class="medium">
-                                    <input name="terms" type="checkbox"> Estou de acordo com os <a data-toggle="modal" href="#termosDeUso">Termos de Uso.</a>
-                                </label>
+                                <label class="custom-control custom-checkbox">
+                                    <input class="custom-control-input" type="checkbox">
+                                    <span class="custom-control-label align-middle">Estou de acordo com os <a data-toggle="modal" href="#termosDeUso">Termos de Uso.</a></span>
+                                    </label>
                             </div>
                             <input class="btn btn-md btn-primary btn-block" value="Cadastrar" type="submit">
                         </fieldset>
